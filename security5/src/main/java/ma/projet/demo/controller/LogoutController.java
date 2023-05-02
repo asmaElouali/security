18e91bdabedf6dbef246fe812d829d20e2c2dafb
asma <asmaelouali372@gmail.com>
2023-05-02 02:14:52 +0200

    first commit

---
+++ security5/src/main/java/ma/projet/demo/controller/LogoutController.java
@@ -0,0 +1,12 @@
+package ma.projet.demo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LogoutController {
+	@GetMapping("/logout")
+	public String login() {
+		return "logout";
+	}
+}
